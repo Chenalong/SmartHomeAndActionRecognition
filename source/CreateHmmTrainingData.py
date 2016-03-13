d618258e369a595e3566a2c7e844d8d731388141
Chenalong <562963835@qq.com>
2016-03-13 16:19:09 +0800

    add data_clean
    数据清洗

--- source/CreateHmmTrainingData.py
+++ source/CreateHmmTrainingData.py
@@ -3,8 +3,21 @@
 生成HMM方法所需的数据格式的文件
 """
 
+
 class hmm_training_data():
     """
-        data_num : 表示当前记录
+        data_num : 表示一天记录数
+        data_list : 记录列表,列表中的元素是字典｛time_field_segment：value,device_id=value,device_status=value｝
     """
-    def __int__(self,data_num,time_segment,device_id,status):
+
+    def __int__(self):
+        data_num = 0
+        data_list = list()
+        feature_list = list()
+
+    def add_data(self, time_field_segment_value, device_id_value, device_status_value):
+        data_list.append({"time_field_segment": time_field_segment_value, "device_id": device_id_value, "device_status":
+                         device_status_value})
+        data_num += 1
+
+    def
